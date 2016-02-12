5ac1db6a31f6e20f2923e3eca729b50df171cf63
Shannon Nachreiner <shannonnachreiner2012@u.northwestern.edu>
2016-02-12 17:00:00 -0600

    started work on participation

---
+++ ce-app/ParticipateController.swift
@@ -0,0 +1,64 @@
+//
+//  ParticipateController.swift
+//  ce-app
+//
+//  Created by Shannon Nachreiner on 2/12/16.
+//  Copyright © 2016 delta. All rights reserved.
+//
+
+import UIKit
+
+class ParticipateController: UIViewController,UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate {
+    var meteorClient: MeteorClient!
+    let expId = "7XMzsFoXSoHq8HnH3"
+    
+    
+    @IBOutlet weak var experienceNameLabel: UILabel!
+    @IBOutlet weak var experienceDescLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func uploadPictureButtonTapped(sender: UIButton) {
+        
+    }
+    
+    let picker = UIImagePickerController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        meteorClient = (UIApplication.sharedApplication().delegate as! AppDelegate).meteorClient
+        
+        picker.delegate = self
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "meteorClientConnected", name: MeteorClientConnectionReadyNotification, object: nil)
+        
+        
+    }
+    
+    @IBAction func shootPhotoButtonTapped(sender: UIButton) {
+        
+    }
+    
+    func meteorClientConnected() {
+        let params: [AnyObject] = [["_id": expId]]
+        meteorClient.callMethodName("getExperiences", parameters: params) { (response, error) -> Void in
+            if let result = response {
+                let experience = result["result"]![0] as! [String:AnyObject]
+                self.experienceNameLabel.text = experience["name"] as! String
+                self.experienceDescLabel.text = experience["description"] as! String
+            }
+        }
+    }
+    
+    //MARK: Delegates
+    func imagePickerController(
+        picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        
+    }
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        
+    }
+}
